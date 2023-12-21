532578caa3da2a6e120fdca39d0ac96fd3035116
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-12-20 22:15:47 -0800

    testing literal path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,19 @@
-// Navbar.tsx
 import { Link } from "react-router-dom";
 import { logo } from "../../public/dumbbell.tsx";
 
 const Navbar = () => {
-  const basePath = import.meta.env.BASE_URL;
-  console.log(basePath);
-
   return (
     <nav className="navbar">
       <div className="logo">{logo}</div>
       <ul className="nav-links">
         <li>
-          <Link to={`${basePath}/home`}>Home</Link>
+          <Link to="/FootScheduler/home">Home</Link>
         </li>
         <li>
-          <Link to={`${basePath}/schedule`}>Schedule</Link>
+          <Link to="/FootScheduler/schedule">Schedule</Link>
         </li>
         <li>
-          <Link to={`${basePath}/about`}>About</Link>
+          <Link to="/FootScheduler/about">About</Link>
         </li>
       </ul>
     </nav>
