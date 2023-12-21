f0e143ca7255584d2b1d239952f22bf47aaa6329
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-12-20 23:15:10 -0800

    fix path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,24 +5,21 @@ const Navbar = () => {
   const homeLink = `${import.meta.env["BASE_URL"]}home`;
   const scheduleLink = `${import.meta.env["BASE_URL"]}schedule`;
   const aboutLink = `${import.meta.env["BASE_URL"]}about`;
-  console.log("BASE_URL: ", import.meta.env["BASE_URL"]);
-  console.log(homeLink, scheduleLink, aboutLink);
-  return (
-    <nav className="navbar">
-      <div className="logo">{logo}</div>
-      <ul className="nav-links">
-        <li>
-          <Link to={homeLink}>Home</Link>
-        </li>
-        <li>
-          <Link to={scheduleLink}>Schedule</Link>
-        </li>
-        <li>
-          <Link to={aboutLink}>About</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+
+  <nav className="navbar">
+    <div className="logo">{logo}</div>
+    <ul className="nav-links">
+      <li>
+        <Link to={homeLink}>Home</Link>
+      </li>
+      <li>
+        <Link to={scheduleLink}>Schedule</Link>
+      </li>
+      <li>
+        <Link to={aboutLink}>About</Link>
+      </li>
+    </ul>
+  </nav>;
 };
 
 export default Navbar;
