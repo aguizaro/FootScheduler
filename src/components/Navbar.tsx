ebccd34bae7d2b3119a0e318af1678e729c6dbac
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-12-20 23:18:59 -0800

    fix nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,24 +2,26 @@ import { Link } from "react-router-dom";
 import { logo } from "../../public/dumbbell.tsx";
 
 const Navbar = () => {
-  const homeLink = `${import.meta.env["BASE_URL"]}home`;
-  const scheduleLink = `${import.meta.env["BASE_URL"]}schedule`;
-  const aboutLink = `${import.meta.env["BASE_URL"]}about`;
+  const homeLink = `${import.meta.env["BASE_URL"]}/home`;
+  const scheduleLink = `${import.meta.env["BASE_URL"]}/schedule`;
+  const aboutLink = `${import.meta.env["BASE_URL"]}/about`;
 
-  <nav className="navbar">
-    <div className="logo">{logo}</div>
-    <ul className="nav-links">
-      <li>
-        <Link to={homeLink}>Home</Link>
-      </li>
-      <li>
-        <Link to={scheduleLink}>Schedule</Link>
-      </li>
-      <li>
-        <Link to={aboutLink}>About</Link>
-      </li>
-    </ul>
-  </nav>;
+  return (
+    <nav className="navbar">
+      <div className="logo">{logo}</div>
+      <ul className="nav-links">
+        <li>
+          <Link to={homeLink}>Home</Link>
+        </li>
+        <li>
+          <Link to={scheduleLink}>Schedule</Link>
+        </li>
+        <li>
+          <Link to={aboutLink}>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
